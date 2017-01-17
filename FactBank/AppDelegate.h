4d638a79089c851aad832e05c67afaaf20fcc7b4
John Daratzikis <johndaratzikis@johns-macbook-pro.local>
2017-01-17 19:10:15 +0200

    Initial Commit

---
+++ FactBank/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FactBank
+//
+//  Created by John Daratzikis on 17/01/2017.
+//  Copyright © 2017 ioannisdaratzikis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FactBank/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FactBank
+//
+//  Created by John Daratzikis on 17/01/2017.
+//  Copyright © 2017 ioannisdaratzikis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
